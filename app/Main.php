9dcc5a9151ba65209f7b16441a0d501e63760036
栗原景子 <kuriharakeiko@kuriharakeikonoMacBook-puro.local>
2018-04-29 20:05:30 +0900

    test1

--- app/Main.php
+++ app/Main.php
@@ -2,7 +2,9 @@
 
 function run ($argc, $argv)
 {
+  var_dump($argc, $argv);
   foreach ($argv as $index=>$value) {
-    printf("argv[%s]: %s\n", $index, $value);
+    #printf("argv[%s]: %s\n", $index, $value);
+    printf("Hello "."%s"."!"."\n", $value);
   }
 }
